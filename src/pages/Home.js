d4ea6e9eec789b3d6d949ebad8882c57973c9939
SallaPR <salla.pr@hotmail.com>
2025-11-04 00:34:32 +0200

    add devicons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import Projects from "../components/Projects"
+import Projects from "../components/Projects";
 import "./Home.css";
 
 const Home = () => {
@@ -6,28 +6,74 @@ const Home = () => {
     <>
       <section id="about">
         <p>
-          I'm a third-year ICT Engineering student in Oulu University of Applied
-          Sciences. I enjoy working on both front-end and back-end technologies, trying
-          out new frameworks, and learning how different technologies interact.
+          I'm a third-year ICT Engineering student in Oulu University of Applied Sciences.
+          I enjoy working on both front-end and back-end technologies, trying out new
+          frameworks, and learning how different technologies interact.
         </p>
-        <h3>Skills</h3>
-        <ul>
-          <li>
-            <b>Programming Languages:</b> C++, JavaScript, Java
-          </li>
-          <li>
-            <b>Web Development:</b> HTML, CSS, React{" "}
-          </li>
-          <li>
-            <b>Mobile Development:</b> React Native, Kotlin
-          </li>
-          <li>
-            <b>Databases:</b> PostgreSQL, MySQL, Firebase
-          </li>
-          <li>
-            <b>Other Technologies:</b> Arduino, Qt, Visual Studio Code
-          </li>
-        </ul>
+        <p>
+          Technologies I've worked with through university projects, personal apps and
+          coursework. Currently building more experience and expanding my full-stack
+          skills.
+        </p>
+
+        <div className="skills">
+          <div className="skill">
+            <i className="devicon-cplusplus-plain skill-icon" />
+            <span className="skill-text">C++</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-javascript-plain skill-icon" />
+            <span className="skill-text">Javascript</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-java-plain skill-icon" />
+            <span className="skill-text">Java</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-html5-plain skill-icon" />
+            <span className="skill-text">HTML</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-css3-plain skill-icon" />
+            <span className="skill-text">CSS</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-react-plain skill-icon" />
+            <span className="skill-text">React</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-reactnative-original skill-icon" />
+            <span className="skill-text">React Native</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-kotlin-plain skill-icon" />
+            <span className="skill-text">Kotlin</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-nodejs-plain skill-icon" />
+            <span className="skill-text">Node.js</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-expo-plain skill-icon" />
+            <span className="skill-text">Expo</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-postgresql-plain skill-icon" />
+            <span className="skill-text">PostgreSQL</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-mysql-original skill-icon" />
+            <span className="skill-text">MySQL</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-firebase-plain skill-icon" />
+            <span className="skill-text">Firebase</span>
+          </div>
+          <div className="skill">
+            <i className="devicon-vscode-plain skill-icon" />
+            <span className="skill-text">Visual Studio Code</span>
+          </div>
+        </div>
       </section>
 
       <Projects />
