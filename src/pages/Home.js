63edecc5618062334afa9ede9a1b92a587922524
SallaPR <salla.pr@hotmail.com>
2025-11-05 15:56:22 +0200

    added image to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,73 +5,80 @@ const Home = () => {
   return (
     <>
       <section id="about">
-        <p>
-          I'm a third-year ICT Engineering student in Oulu University of Applied Sciences.
-          I enjoy working on both front-end and back-end technologies, trying out new
-          frameworks, and learning how different technologies interact.
-        </p>
-        <p>
-          Technologies I've worked with through university projects, personal apps and
-          coursework. Currently building more experience and expanding my full-stack
-          skills.
-        </p>
+        <div className="about-container">
+          <div className="about-content">
+            <p>
+              I'm a third-year ICT Engineering student in Oulu University of Applied
+              Sciences. I enjoy working on both front-end and back-end technologies,
+              trying out new frameworks, and learning how different technologies interact.
+            </p>
+            <p>
+              Technologies I've worked with through university projects, personal apps and
+              coursework. Currently building more experience and expanding my full-stack
+              skills.
+            </p>
 
-        <div className="skills">
-          <div className="skill">
-            <i className="devicon-cplusplus-plain skill-icon" />
-            <span className="skill-text">C++</span>
+            <div className="skills">
+              <div className="skill">
+                <i className="devicon-cplusplus-plain skill-icon" />
+                <span className="skill-text">C++</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-javascript-plain skill-icon" />
+                <span className="skill-text">Javascript</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-java-plain skill-icon" />
+                <span className="skill-text">Java</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-html5-plain skill-icon" />
+                <span className="skill-text">HTML</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-css3-plain skill-icon" />
+                <span className="skill-text">CSS</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-react-plain skill-icon" />
+                <span className="skill-text">React</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-reactnative-original skill-icon" />
+                <span className="skill-text">React Native</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-kotlin-plain skill-icon" />
+                <span className="skill-text">Kotlin</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-nodejs-plain skill-icon" />
+                <span className="skill-text">Node.js</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-expo-plain skill-icon" />
+                <span className="skill-text">Expo</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-postgresql-plain skill-icon" />
+                <span className="skill-text">PostgreSQL</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-mysql-original skill-icon" />
+                <span className="skill-text">MySQL</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-firebase-plain skill-icon" />
+                <span className="skill-text">Firebase</span>
+              </div>
+              <div className="skill">
+                <i className="devicon-vscode-plain skill-icon" />
+                <span className="skill-text">Visual Studio Code</span>
+              </div>
+            </div>
           </div>
-          <div className="skill">
-            <i className="devicon-javascript-plain skill-icon" />
-            <span className="skill-text">Javascript</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-java-plain skill-icon" />
-            <span className="skill-text">Java</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-html5-plain skill-icon" />
-            <span className="skill-text">HTML</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-css3-plain skill-icon" />
-            <span className="skill-text">CSS</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-react-plain skill-icon" />
-            <span className="skill-text">React</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-reactnative-original skill-icon" />
-            <span className="skill-text">React Native</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-kotlin-plain skill-icon" />
-            <span className="skill-text">Kotlin</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-nodejs-plain skill-icon" />
-            <span className="skill-text">Node.js</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-expo-plain skill-icon" />
-            <span className="skill-text">Expo</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-postgresql-plain skill-icon" />
-            <span className="skill-text">PostgreSQL</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-mysql-original skill-icon" />
-            <span className="skill-text">MySQL</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-firebase-plain skill-icon" />
-            <span className="skill-text">Firebase</span>
-          </div>
-          <div className="skill">
-            <i className="devicon-vscode-plain skill-icon" />
-            <span className="skill-text">Visual Studio Code</span>
+          <div className="about-image">
+            <img src={`${process.env.PUBLIC_URL}/images/pfp.jpeg`} alt="Profile photo" />
           </div>
         </div>
       </section>
